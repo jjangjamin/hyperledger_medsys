4d27c5933ce20863deeeaaaaabb0bc513f066385
unknown <jaeminim95@gmail.com>
2020-02-25 17:02:06 +0900

    UPLOAD

---
+++ javascript/invoke.js
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+async function main() {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('Jaemin');
+        if (!userExists) {
+            console.log('An identity for the user "Jaemin" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'Jaemin', discovery: { enabled: false } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('medsys');
+
+        // Submit the specified transaction.
+        // createdata transaction - requires 5 argument, ex: ('createdata', 'data12', 'Name', 'Date', 'symptoms', 'Tom')
+        // changedataOwner transaction - requires 2 args , ex: ('changedataOwner', 'data10', 'Dave')
+        await contract.submitTransaction('createdata', 'data1', 'Jaemin', '2020.01.13', 'CORONA 19', 'Jaemin');
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
